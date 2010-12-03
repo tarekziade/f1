8dd1a72f904a6bcd294bc9c5b3a150d797d8a14a
Bryan Clark <clarkbw@gmail.com>
2010-12-03 13:27:34 -0800

    use the context menu to open the panel

--- extensions/jetpack-panel/lib/main.js
+++ extensions/jetpack-panel/lib/main.js
@@ -1,19 +1,26 @@
-const widgets = require("widget");
+const contextMenu = require("context-menu");
 const panels = require("panel");
-//const data = require("self").data;
 
-widgets.add(widgets.Widget({
-  label: "Mozilla F1",
-  image: "http://f1.mozillamessaging.com/favicon.png",
-  panel: panels.Panel({
-    width: 640,
-    height: 320,
-    contentURL: "http://linkdrop.caraveo.com:5000/designs/popup/",
-    //contentScriptURL: [data.url("jquery-1.4.2.min.js"),
-    //                   data.url("panel.js")],
-    contentScriptWhen: "ready",
-    onMessage: function(message) {
-      //require("tab-browser").addTab(message);
-    }
-  })
-}));
+var sharePanel = panels.add(panels.Panel({
+        width: 640,
+        height: 320,
+        contentURL: "http://linkdrop.caraveo.com:5000/designs/popup/",
+        onMessage: function(message) {
+          //require("tab-browser").addTab(message);
+        }
+      }
+));
+
+var f1ShareItem = contextMenu.Item({
+  label: "Share Page...",
+  contentScript: 'on("click", function (node, data) {' +
+                 '  postMessage(document.URL);' +
+                 '});',
+  onMessage: function (documentURL) {
+    //for(var i = 0; i < metas.length; i++) {
+    //  console.log(metas[i].getAttribute("property"))
+    //}
+    sharePanel.show();
+  }
+});
+contextMenu.add(f1ShareItem);
