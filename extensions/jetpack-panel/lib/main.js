1aa3c928af824a940f464e0e8ff19f77a24383ca
Bryan Clark <clarkbw@gmail.com>
2010-12-03 17:08:45 -0800

    grab the page data at the time of clicking on the context menu and then pass that data to the content script via the postMessage function

--- extensions/jetpack-panel/lib/main.js
+++ extensions/jetpack-panel/lib/main.js
@@ -1,6 +1,7 @@
 const contextMenu = require("context-menu");
 const panels = require("panel");
 const data = require("self").data;
+const tabs = require("tabs");
 
 var sharePanel = panels.add(panels.Panel({
   width: 640,
@@ -29,6 +30,47 @@ var f1ShareItem = contextMenu.Item({
                  '});',
   onMessage: function (documentURL) {
     sharePanel.show();
+
+    var doc = tabs.activeTab.contentDocument;
+    var data = { "url" : doc.URL }
+    var metas = [];
+    try {
+      metas = doc.querySelectorAll("meta[name]");
+      console.log("metas: " + metas.length);
+      for (var i = 0; i < metas.length; i++ ) {
+        var prop = metas[i].getAttribute("name");
+        data[prop] = metas[i].getAttribute("content");
+      }
+    } catch (ignore) {}
+
+    try {
+      metas = doc.querySelectorAll("meta[property]");
+      console.log("metas: " + metas.length);
+
+      for (var i = 0; i < metas.length; i++ ) {
+        var prop = metas[i].getAttribute("property")
+        if (/^og:/.test(prop))
+          prop = prop.replace(/^og:/,"");
+        console.log(prop);
+        data[prop] = metas[i].getAttribute("content");
+      }
+    } catch (ignore) {}
+
+    try {
+      metas = doc.querySelectorAll("link[rel=image_src]");
+      console.log("metas: " + metas.length);
+
+      for (var i = 0; i < metas.length; i++ ) {
+        var prop = metas[i].getAttribute("rel")
+        console.log(prop);
+        data[prop] = metas[i].getAttribute("href");
+      }
+    } catch (ignore) {}
+
+
+    console.log("data: " + data);
+
+    sharePanel.postMessage(JSON.stringify(data));
   }
 });
 contextMenu.add(f1ShareItem);
